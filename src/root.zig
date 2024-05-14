a1cda610bb182344b7470e812746496d37cdbe67
Oleksii Shevchenko <peace_oleksij@proton.me>
2024-05-14 16:04:09 +0300

    Add simple .map method and MapItarator construct

--- src/root.zig
+++ src/root.zig
@@ -47,6 +47,37 @@ pub fn ZodashArrayList(comptime T: type) type {
             };
         }
 
+        // NOTE: IT - InputType, OT - OutputType
+        pub fn MapIterator(comptime IT: type, comptime OT: type, comptime changer: fn (IT) OT) type {
+            return struct {
+                index: usize,
+                arr: []const IT,
+
+                fn next(this: *@This()) ?OT {
+                    while (this.index < this.arr.len) {
+                        defer this.index += 1;
+                        return changer(this.arr[this.index]);
+                    }
+                    return null;
+                }
+
+                fn init(zarr: ZAType) @This() {
+                    return @This(){ .index = 0, .arr = zarr.arraylist.items };
+                }
+            };
+        }
+
+        pub fn map(zarr: *@This(), comptime changer: fn (T) T) !void {
+            var new_arr = std.ArrayList(T).init(zarr.allocator);
+            var iterator = MapIterator(T, T, changer).init(zarr.*);
+
+            while (iterator.next()) |item|
+                try new_arr.append(item);
+
+            zarr.arraylist.deinit();
+            zarr.arraylist = new_arr;
+        }
+
         pub fn filter(zarr: *@This(), comptime condition: fn (T) ?T) !void {
             var new_arr = std.ArrayList(T).init(zarr.allocator);
             var iterator = FilterIterator(condition).init(zarr.*);
@@ -60,6 +91,11 @@ pub fn ZodashArrayList(comptime T: type) type {
 
         const DefaultStages = union(enum) {
             filter: fn (T) ?T,
+            map: fn (T) T,
+            // clone_map: struct {
+            //     IT: type,
+            //     OT: type,
+            // },
         };
 
         pub fn exec(this: *@This(), comptime stages: anytype) !void {
@@ -67,6 +103,8 @@ pub fn ZodashArrayList(comptime T: type) type {
                 const switchable = @as(DefaultStages, stg);
                 switch (switchable) {
                     .filter => |func| try this.filter(func),
+                    .map => |func| try this.map(func),
+                    // .clone_map => @compileError("Use .map but not .clone_map for method exec()."),
                 }
             }
         }
@@ -79,6 +117,7 @@ pub fn ZodashArrayList(comptime T: type) type {
                 const last_stage = stages[stages.len - 1];
                 const unpacked_last_stage = switch (@as(DefaultStages, last_stage)) {
                     .filter => |func| func,
+                    .map => |func| func,
                 };
                 const last_stage_return_type = @typeInfo(@TypeOf(unpacked_last_stage)).Fn.return_type orelse void;
 
@@ -101,6 +140,7 @@ pub fn ZodashArrayList(comptime T: type) type {
                 fn nextFunc(value: anytype, comptime index: comptime_int) ?ResultType {
                     const func = switch (@as(DefaultStages, stages[index])) {
                         .filter => |fnc| fnc,
+                        .map => |fnc| fnc,
                     };
                     const is_last_iteration = index + 1 == stages.len;
                     const new_value = func(value);
@@ -157,6 +197,55 @@ test "FilterIterator" {
     try std.testing.expectEqual(iterator.next().?, 4);
     try std.testing.expectEqual(iterator.next().?, 6);
     try std.testing.expectEqual(iterator.next(), null);
+    try std.testing.expectEqual(iterator.next(), null);
+}
+
+fn addOne(num: u8) u8 {
+    return num + 1;
+}
+
+test "Map" {
+    var arr = ZodashArrayList(u8).init(std.testing.allocator);
+    defer arr.deinit();
+
+    try arr.arraylist.appendSlice(&[_]u8{ 1, 2, 3, 4, 5, 6 });
+
+    try arr.map(addOne);
+
+    try std.testing.expectEqualSlices(u8, arr.arraylist.items, &[_]u8{ 2, 3, 4, 5, 6, 7 });
+}
+
+test "MapIterator 1" {
+    var arr = ZodashArrayList(u8).init(std.testing.allocator);
+    defer arr.deinit();
+
+    try arr.arraylist.appendSlice(&[_]u8{ 1, 2, 3 });
+
+    var iterator = ZodashArrayList(u8).MapIterator(u8, u8, addOne).init(arr);
+
+    try std.testing.expectEqual(iterator.next().?, 2);
+    try std.testing.expectEqual(iterator.next().?, 3);
+    try std.testing.expectEqual(iterator.next().?, 4);
+    try std.testing.expectEqual(iterator.next(), null);
+    try std.testing.expectEqual(iterator.next(), null);
+}
+
+fn asU16(num: u8) u16 {
+    return @as(u16, num);
+}
+
+test "MapIterator 2" {
+    var arr = ZodashArrayList(u8).init(std.testing.allocator);
+    defer arr.deinit();
+
+    try arr.arraylist.appendSlice(&[_]u8{ 1, 2, 3 });
+
+    var iterator = ZodashArrayList(u8).MapIterator(u8, u16, asU16).init(arr);
+
+    try std.testing.expectEqual(iterator.next().?, @as(u16, 1));
+    try std.testing.expectEqual(iterator.next().?, @as(u16, 2));
+    try std.testing.expectEqual(iterator.next().?, @as(u16, 3));
+    try std.testing.expectEqual(iterator.next(), @as(?u16, null));
     try std.testing.expectEqual(iterator.next(), null);
 }
 
